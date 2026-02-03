1f8bf4a1aedbae28887e74533f34f8b8c2abcd38
trainer1-computhink <trainer1@computhink.com.sg>
2026-02-03 06:43:09 +0000

    suggestion

--- CT08_03/project3_restaurantmenu1.py
+++ CT08_03/project3_restaurantmenu1.py
@@ -22,7 +22,12 @@ while item != "no more":
         sure = input(f"You sure you want ah? The {item} costs {price} :").lower().strip()
 
         if sure == "yes":
+            # need to check for the item is it already in the eating
+            # if already in the eating
+            # eating[item] += price because if you do not do these you will only overwrite the entry
+            # else the below
             eating[item] = price
+
             
 
             if price > dolla:
