b169bb638e26bc8df47baf3832169ce4d1dbfacf
Thinknoodles2012Fan <Kayvengst@gmail.com>
2026-04-05 02:45:29 +0000

    End of lesson 8a

--- CT08_08/project8_vowelsherlock.py
+++ CT08_08/project8_vowelsherlock.py
@@ -1,7 +1,7 @@
 import os
 
 filepath = os.getcwd()
-fullpath = os.path.join(filepath, "sherlock.txt")
+fullpath = os.path.join(filepath, "CT08_08/sherlock.txt")
 
 if not os.path.exists(fullpath):
     print("There is no such thing called sherlock.txt")
@@ -14,10 +14,39 @@ print(f"Number of character in the text : {len(lines)}")
 
 vowels = {
     "a" : 0,
-    "b" : 0,
-    "c" : 0,
-    "d" : 0,
-    "e" : 0
+    "e" : 0,
+    "i" : 0,
+    "o" : 0,
+    "u" : 0
 }
 
+total_vowels = 0
 
+for char in lines.lower():
+    if char in vowels:
+        vowels[char] += 1
+        total_vowels += 1
+
+print("\nVowel Counts:")
+for v in vowels:
+    print(f"{v} : {vowels[v]}")
+
+print(f"Total vowels: {total_vowels}")
+
+# Task 4: Calculate percentage
+percentage = (total_vowels / len(lines)) * 100
+print((f"Percentage of vowels: {percentage:.2f}%"))
+
+# Task 5: Write results to a new file
+output = open("vowel_counts.txt", "w")
+
+output.write("Vowel Counts:")
+for v in vowels:
+    output.write("\n" + v + " : " + str(vowels[v]))
+
+output.write("\nTotal vowels: " + str(total_vowels))
+output.write("\nPercentage of vowels: {:.2f}%".format(percentage))
+
+output.close()
+
+print("\nResults saved to vowel_counts.txt successfully.")
